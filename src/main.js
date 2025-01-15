d94cda636e8c97e39ebe2c207c049a08eaa3be02
xOAviOx <avishuklacode@gmail.com>
2025-01-15 22:28:44 +0530

    New functions

--- src/main.js
+++ src/main.js
@@ -6,14 +6,38 @@ const k = kaplay();
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadSprite("bean", "sprites/bean.png");
 
-// k.add([k.pos(120, 80), k.sprite("bean")]);   
+// k.add([k.pos(120, 80), k.sprite("bean")]);
 
 // putting together our player character
-const bean = add([sprite("bean"), pos(80, 40), area(), body()]);
+const bean = k.add([sprite("bean"), pos(80, 40), area(), body()]);
 
-// .jump() when "space" key is pressed
-onKeyPress("space", () => {
-  bean.jump();
+// add platform
+k.add([
+  rect(width(), 48),
+  pos(0, height() - 48),
+  outline(4),
+  area(),
+  body({ isStatic: true }),
+  color(127, 200, 255),
+]);
+
+k.setGravity(1600);
+
+//jump only if grounded
+
+k.onKeyPress("space", () => {
+  if (bean.isGrounded()) {
+    bean.jump();
+  }
 });
-
-//platform
+// add tree
+k.add([
+  rect(48, 64),
+  area(),
+  outline(4),
+  pos(width(), height() - 48),
+  anchor("botleft"),
+  color(255, 180, 255),
+  move(LEFT, 240),
+  "tree"
+]);
