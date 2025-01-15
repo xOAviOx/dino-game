05ec100e4929e3fab618f0688fa8b561cb8b86b7
xOAviOx <avishuklacode@gmail.com>
2025-01-15 22:50:54 +0530

    Game functionalities

--- src/main.js
+++ src/main.js
@@ -1,43 +1,108 @@
 import kaplay from "kaplay";
 // import "kaplay/global"; // uncomment if you want to use without the k. prefix
+const FLOOR_HEIGHT = 48;
+const JUMP_FORCE = 800;
+const SPEED = 480;
 
-const k = kaplay();
+// initialize context
+kaplay();
 
-k.loadRoot("./"); // A good idea for Itch.io publishing later
-k.loadSprite("bean", "sprites/bean.png");
+// load assets
+loadSprite("bean", "sprites/bean.png");
 
-// k.add([k.pos(120, 80), k.sprite("bean")]);
+scene("game", () => {
+    // define gravity
+    setGravity(1600);
 
-// putting together our player character
-const bean = k.add([sprite("bean"), pos(80, 40), area(), body()]);
+    // add a game object to screen
+    const player = add([
+        // list of components
+        sprite("bean"),
+        pos(80, 40),
+        area(),
+        body(),
+    ]);
 
-// add platform
-k.add([
-  rect(width(), 48),
-  pos(0, height() - 48),
-  outline(4),
-  area(),
-  body({ isStatic: true }),
-  color(127, 200, 255),
-]);
+    // floor
+    add([
+        rect(width(), FLOOR_HEIGHT),
+        outline(4),
+        pos(0, height()),
+        anchor("botleft"),
+        area(),
+        body({ isStatic: true }),
+        color(127, 200, 255),
+    ]);
 
-k.setGravity(1600);
+    function jump() {
+        if (player.isGrounded()) {
+            player.jump(JUMP_FORCE);
+        }
+    }
 
-//jump only if grounded
+    // jump when user press space
+    onKeyPress("space", jump);
+    onClick(jump);
 
-k.onKeyPress("space", () => {
-  if (bean.isGrounded()) {
-    bean.jump();
-  }
+    function spawnTree() {
+        // add tree obj
+        add([
+            rect(48, rand(32, 96)),
+            area(),
+            outline(4),
+            pos(width(), height() - FLOOR_HEIGHT),
+            anchor("botleft"),
+            color(255, 180, 255),
+            move(LEFT, SPEED),
+            "tree",
+        ]);
+
+        // wait a random amount of time to spawn next tree
+        wait(rand(0.5, 1.5), spawnTree);
+    }
+
+    // start spawning trees
+    spawnTree();
+
+    // lose if player collides with any game obj with tag "tree"
+    player.onCollide("tree", () => {
+        // go to "lose" scene and pass the score
+        go("lose", score);
+        burp();
+        addKaboom(player.pos);
+    });
+
+    // keep track of score
+    let score = 0;
+
+    const scoreLabel = add([text(score), pos(24, 24)]);
+
+    // increment score every frame
+    onUpdate(() => {
+        score++;
+        scoreLabel.text = score;
+    });
 });
-// add tree
-k.add([
-  rect(48, 64),
-  area(),
-  outline(4),
-  pos(width(), height() - 48),
-  anchor("botleft"),
-  color(255, 180, 255),
-  move(LEFT, 240),
-  "tree"
-]);
+
+scene("lose", (score) => {
+    add([
+        sprite("bean"),
+        pos(width() / 2, height() / 2 - 80),
+        scale(2),
+        anchor("center"),
+    ]);
+
+    // display score
+    add([
+        text(score),
+        pos(width() / 2, height() / 2 + 80),
+        scale(2),
+        anchor("center"),
+    ]);
+
+    // go back to game with space is pressed
+    onKeyPress("space", () => go("game"));
+    onClick(() => go("game"));
+});
+
+go("game");
